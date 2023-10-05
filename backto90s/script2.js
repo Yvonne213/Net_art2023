302ba44209e23fb5f52aff9d67748b86d92de4b4
Yvonne213 <hywener@gmail.com>
2023-10-04 22:07:31 -0500

    add third page

--- backto90s/script2.js
+++ backto90s/script2.js
@@ -1,4 +1,5 @@
 document.body.addEventListener('click', function() {
+    console.log("Body clicked!");
     var thirdWindow = window.open('windows3.html', '_blank', 'width=400,height=250,top=700,left=990');
 
 });
