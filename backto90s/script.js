86035023ec7f9c31e6eb6f56fbdb6e5e2fc77ed4
Yvonne213 <hywener@gmail.com>
2023-10-04 21:44:15 -0500

    two windows pages

---
+++ backto90s/script.js
@@ -0,0 +1,4 @@
+document.getElementById('myImage').addEventListener('click', function() {
+    var newWindow = window.open('windows2.html', '_blank', 'width=600,height=400')
+
+});
---
+++ backto90s/script2.js
@@ -0,0 +1,4 @@
+document.body.addEventListener('click', function() {
+    var thirdWindow = window.open('windows3.html', '_blank', 'width=400,height=250,top=700,left=990');
+
+});
